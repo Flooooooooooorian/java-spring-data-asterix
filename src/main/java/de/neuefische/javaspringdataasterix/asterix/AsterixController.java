51a0a0a116200f3c7d442c02233c6526eb03a2da
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-12-21 15:27:03 +0100

    Add QueryByExample

--- src/main/java/de/neuefische/javaspringdataasterix/asterix/AsterixController.java
+++ src/main/java/de/neuefische/javaspringdataasterix/asterix/AsterixController.java
@@ -16,9 +16,54 @@ public class AsterixController {
         this.characterRepository = characterRepository;
     }
 
-    @GetMapping("/api/asterix/characters")
-    public List<Character> getAllCharacters() {
-        return characterRepository.findAll();
+    @GetMapping("/api/asterix/characters1")
+    public List<Character> getAllCharactersWithStream(@RequestParam(required = false) String name,
+                                            @RequestParam(required = false) Integer age,
+                                            @RequestParam(required = false) String profession) {
+
+        return characterRepository.findAll().stream()
+                .filter(character -> name == null || character.name().equals(name))
+                .filter(character -> age == null || character.age().equals(age))
+                .filter(character -> profession == null || character.profession().equals(profession))
+                .toList();
+    }
+
+    @GetMapping("/api/asterix/characters2")
+    public List<Character> getAllCharactersWithRepository(@RequestParam(required = false) String name,
+                                            @RequestParam(required = false) Integer age,
+                                            @RequestParam(required = false) String profession) {
+        if (name == null && age == null && profession == null) {
+            return characterRepository.findAll();
+        }
+        else if (name != null && age == null && profession == null) {
+            return characterRepository.findCharactersByName(name);
+        }
+        else if (name == null && age != null && profession == null) {
+            return characterRepository.findCharactersByAge(age);
+        }
+        else if (name != null && age != null && profession == null) {
+            return characterRepository.findCharactersByNameAndAge(name, age);
+        }
+        else if (name == null && age == null) {
+            return characterRepository.findCharactersByProfession(profession);
+        }
+        else if (name != null && age == null) {
+            return characterRepository.findCharactersByNameAndProfession(name, profession);
+        }
+        else if (name == null) {
+            return characterRepository.findCharactersByAgeAndProfession(age, profession);
+        }
+        else {
+            return characterRepository.findCharactersByNameAndAgeAndProfession(name, age, profession);
+        }
+    }
+
+    @GetMapping("/api/asterix/characters3")
+    public List<Character> getAllCharactersWithExample(@RequestParam(required = false) String name,
+                                            @RequestParam(required = false) Integer age,
+                                            @RequestParam(required = false) String profession) {
+
+        return characterRepository.findAll(Example.of(new Character(null, name, age, profession)));
     }
 
     @PostMapping("/api/asterix/characters")
--- src/main/java/de/neuefische/javaspringdataasterix/asterix/CharacterRepository.java
+++ src/main/java/de/neuefische/javaspringdataasterix/asterix/CharacterRepository.java
@@ -2,5 +2,15 @@ package de.neuefische.javaspringdataasterix.asterix;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface CharacterRepository extends MongoRepository<Character, String> {
+
+    List<Character> findCharactersByNameAndAgeAndProfession(String name, Integer age, String profession);
+    List<Character> findCharactersByNameAndAge(String name, Integer age);
+    List<Character> findCharactersByNameAndProfession(String name, String profession);
+    List<Character> findCharactersByAgeAndProfession(Integer age, String profession);
+    List<Character> findCharactersByAge(Integer age);
+    List<Character> findCharactersByName(String name);
+    List<Character> findCharactersByProfession(String profession);
 }
